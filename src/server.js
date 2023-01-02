09312f5f4deb4c8a115f48a7dc08f746ee50287d
kimjuno97 <jh241093@gmail.com>
2023-01-02 11:20:30 +0900

    dotenv 추가 및, 환경변수 세팅

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import { off } from 'process';
+import dotenv from 'dotenv';
+
+dotenv.config({
+	path: '.env.development',
+});
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ['http://localhost:8080', 'http://10.58.52.111:8080'],
-		// allowedHeaders: ['my-custom-header'],
+		origin: [process.env.NODE_ENV_LOCAL_IP, process.env.NODE_ENV_WIFI_IP],
 		credentials: true,
 	},
 });
@@ -22,7 +25,8 @@ app.get('/', (req, res) => {
 
 // socket 통신
 io.on('connection', socket => {
-	console.log('연결됨');
+	console.log('welcome socket connection');
+
 	socket.on('join_room', roomName => {
 		console.log('romm', roomName);
 		socket.join(roomName);
